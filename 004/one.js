2be60b997d43756356ea9da9fb62ebec847204b0
paullabkorea <pixarlove@naver.com>
2021-10-15 10:46:55 +0900

    second

---
+++ 004/one.js
@@ -0,0 +1,10 @@
+import * as 개인프로필 from './two.js';
+// const 개인프로필 = require('./two');
+
+
+console.log(개인프로필.이름);
+console.log(개인프로필.나이);
+개인프로필.한살더먹음();
+개인프로필.한살더먹음();
+console.log(개인프로필.나이); //이번에는 출력이 되었죠?
+console.log(개인프로필.값가져와());
---
+++ 005/app.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+console.log(`구분자 : ${path.sep}`);
+console.log(`디렉토리 : ${path.dirname(__filename)}`);
+console.log(`파일이름 : ${path.basename(__filename)}`);
+console.log(`확장자 : ${path.extname(__filename)}`);
+
+console.log(__filename);
+console.log(__dirname);
+console.table(path.parse(__filename));
+console.log(path.join(__dirname, 'source'))
